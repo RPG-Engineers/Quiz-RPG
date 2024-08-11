392e26eb2948f17c06424c108c139afcf7f086fd
Murilo <lilo.marino@hotmail.com>
2024-08-10 23:57:39 -0300

    feat: :sparkles: Implementado criação de Classe e Raça

--- src/views/Backgrounds.tsx
+++ src/views/Backgrounds.tsx
@@ -1,18 +1,15 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import CaracteristicaCreator from "../components/CaracteristicaCreator";
 import { TipoCaracteristica } from "../types";
 
 const Backgrounds: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleEdit = (id: number) => {
-        navigate(`/editar-background/${id}`); // Navegar para a tela de edição
-    };
+  const handleEdit = (id: number) => {
+    navigate(`/editar-background/${id}`); // Navegar para a tela de edição
+  };
 
-    return (
-        <CaracteristicaCreator tipo={TipoCaracteristica.BACKGROUND} handleEdit={handleEdit}></CaracteristicaCreator>
-    );
+  return <CaracteristicaCreator tipo={TipoCaracteristica.BACKGROUND} handleEdit={handleEdit}></CaracteristicaCreator>;
 };
 
 export default Backgrounds;
