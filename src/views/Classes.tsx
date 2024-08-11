3f0f37e17dc902d0d24bd211019484287b5e0f61
Murilo <lilo.marino@hotmail.com>
2024-08-10 23:57:39 -0300

    feat: :sparkles: Implementado criação de Classe e Raça

--- src/views/Classes.tsx
+++ src/views/Classes.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import CaracteristicaCreator from "../components/CaracteristicaCreator";
+import { TipoCaracteristica } from "../types";
+
 const Classes: React.FC = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleEdit = (id: number) => {
+    navigate(`/editar-classe/${id}`); // Navegar para a tela de edição
+  };
+
+  return <CaracteristicaCreator tipo={TipoCaracteristica.CLASSE} handleEdit={handleEdit}></CaracteristicaCreator>;
 };
 
 export default Classes;
--- src/views/Racas.tsx
+++ src/views/Racas.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import CaracteristicaCreator from "../components/CaracteristicaCreator";
+import { TipoCaracteristica } from "../types";
+
 const Racas: React.FC = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleEdit = (id: number) => {
+    navigate(`/editar-raca/${id}`); // Navegar para a tela de edição
+  };
+
+  return <CaracteristicaCreator tipo={TipoCaracteristica.RACA} handleEdit={handleEdit}></CaracteristicaCreator>;
 };
 
 export default Racas;
