4451df8d315473b43eda1c9a6f8bab8dae7c64e3
LiloMarino <lilo.marino@hotmail.com>
2024-09-03 16:36:30 -0300

    feat: :lipstick: Feito front-end de Questionário

--- src/views/Questionarios.tsx
+++ src/views/Questionarios.tsx
@@ -1,5 +1,29 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { CreateQuiz } from "../components/CreateQuiz";
+import { CardQuiz } from "../components/CardQuiz";
+
 const Questionarios: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <Container className="h-100 mt-3">
+        <Row className="align-items-center h-100">
+          <Col xs={6} className="mx-auto">
+            <Card className="pt-4">
+              <Card.Body className="text-center">
+                <FontAwesomeIcon icon={faPlus} size="2x" className="text-body-secondary" />
+                <p className="text-body-secondary">Criar novo questionário</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <CardQuiz title="Questionário Jogador" checked={true} />
+      <CardQuiz title="Questionário NPC" checked={false} />
+      <CreateQuiz />
+    </>
+  );
 };
 
 export default Questionarios;
