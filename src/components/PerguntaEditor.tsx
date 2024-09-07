49c77f8f409edd07cfaf35576acef4eee86b95f1
Murilo <lilo.marino@hotmail.com>
2024-09-07 19:07:30 -0300

    feat: :sparkles: Implementado Editar Pergunta

--- src/components/PerguntaEditor.tsx
+++ src/components/PerguntaEditor.tsx
@@ -1,19 +1,133 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Container, Row, Col, Card, Form, Button, Accordion } from "react-bootstrap";
 import { CreateAlternativa, CreateAlternativaProps } from "./CreateAlternativa";
+import { v4 as uuidv4 } from "uuid";
+import { getPerguntaById, getAlternativasWithTagsByPergunta, editPerguntaEAlternativas } from "../database/database";
+import { AlternativaWithTags, Tag } from "../types";
+import { useNavigate } from "react-router-dom";
 
-export const PerguntaEditor: React.FC = () => {
-  const [alternativas, setAlternativas] = useState<CreateAlternativaProps[]>([
-    { placeholder: "Opção 1", eventKey: "0" },
-  ]);
+interface PerguntaEditorProps {
+  id: number;
+  tags: Tag[];
+  navigationDestiny: string;
+}
+
+export const PerguntaEditor: React.FC<PerguntaEditorProps> = ({ id, tags, navigationDestiny }) => {
+  const [alternativaProps, setAlternativaProps] = useState<CreateAlternativaProps[]>([]);
+  const [questionText, setQuestionText] = useState("");
+  const [alternativaTexts, setAlternativaTexts] = useState<{ [key: string]: string }>({});
+  const [alternativaTags, setAlternativaTags] = useState<{ [key: string]: Set<number> }>({});
+  const navigate = useNavigate();
 
   const handleAddOption = () => {
-    const newKey = alternativas.length.toString();
-    const newOption = {
-      placeholder: `Opção ${alternativas.length + 1}`,
-      eventKey: newKey,
+    const newOptionId = uuidv4();
+    setAlternativaProps((prev) => [
+      ...prev,
+      {
+        id: newOptionId,
+        placeholder: `Opção ${prev.length + 1}`,
+        eventKey: newOptionId,
+        tags: [],
+        onRemove: () => handleRemoveOption(newOptionId),
+        onTextChange: handleAlternativaTextChange,
+        onTagChange: handleAlternativaTagChange,
+      },
+    ]);
+  };
+
+  const handleAlternativaTextChange = (id: string, text: string) => {
+    setAlternativaTexts((prev) => ({ ...prev, [id]: text }));
+  };
+
+  const handleAlternativaTagChange = useCallback((id: string, selectedTags: Set<number>) => {
+    setAlternativaTags((prev) => ({ ...prev, [id]: selectedTags }));
+  }, []);
+
+  const handleRemoveOption = (idToRemove: string) => {
+    setAlternativaProps((prev) =>
+      prev
+        .filter((alt) => alt.id !== idToRemove)
+        .map((alt, index) => ({ ...alt, placeholder: `Opção ${index + 1}`, eventKey: alt.id }))
+    );
+
+    setAlternativaTexts((prev) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [idToRemove]: _, ...rest } = prev;
+      return rest;
+    });
+
+    setAlternativaTags((prev) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [idToRemove]: _, ...rest } = prev;
+      return rest;
+    });
+  };
+
+  useEffect(() => {
+    const fetchPerguntaData = async () => {
+      const pergunta = await getPerguntaById(id);
+      setQuestionText(pergunta.pergunta);
+
+      const alternativasWithTags = await getAlternativasWithTagsByPergunta(pergunta);
+
+      const alternativasTexts: { [key: string]: string } = {};
+      const alternativasTags: { [key: string]: Set<number> } = {};
+      const alternativasProps: CreateAlternativaProps[] = [];
+
+      for (const alternativa of alternativasWithTags) {
+        if (alternativa.id_alternativa) {
+          alternativasTexts[alternativa.id_alternativa.toString()] = alternativa.alternativa;
+          alternativasTags[alternativa.id_alternativa.toString()] = new Set<number>(
+            alternativa.tags.map(tag => tag.id_tag!).filter(Boolean)
+          );
+
+          alternativasProps.push({
+            id: alternativa.id_alternativa.toString(),
+            placeholder: `Opção ${alternativasProps.length + 1}`,
+            eventKey: alternativa.id_alternativa.toString(),
+            tags: tags,
+            onRemove: () => handleRemoveOption(alternativa.id_alternativa!.toString()),
+            onTextChange: handleAlternativaTextChange,
+            onTagChange: handleAlternativaTagChange,
+            initialText: alternativa.alternativa, 
+            initialTags: new Set(alternativa.tags.map(tag => tag.id_tag!).filter(Boolean)),
+          });
+        }
+      }
+
+      setAlternativaTexts(alternativasTexts);
+      setAlternativaProps(alternativasProps);
+      setAlternativaTags(alternativasTags);
     };
-    setAlternativas([...alternativas, newOption]);
+
+    fetchPerguntaData();
+  }, [id, handleAlternativaTagChange, tags]);
+
+  const handleEdit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    const alternativas: AlternativaWithTags[] = alternativaProps.map((alt) => ({
+      id_alternativa: alt.id !== uuidv4() ? Number(alt.id) : undefined,
+      id_pergunta: id,
+      alternativa: alternativaTexts[alt.id] || "",
+      tags: Array.from(alternativaTags[alt.id] || []).map(tagId => ({
+        id_tag: tagId,
+        nome: "", // Assumindo que o nome e a cor das tags já são conhecidos em outro contexto
+        cor: ""
+      }))
+    }));
+  
+    try {
+      await editPerguntaEAlternativas(
+        { id_pergunta: id, pergunta: questionText },
+        alternativas
+      );
+    } catch (error) {
+      console.error("Erro ao salvar pergunta e alternativas:", error);
+    }
+  
+    // Redirecionar para a página especificada após salvar
+    navigate(navigationDestiny);
   };
   
   return (
@@ -23,27 +137,41 @@ export const PerguntaEditor: React.FC = () => {
           <Card>
             <Card.Body>
               <Card.Title>Sua Pergunta</Card.Title>
-              <Form id="questionForm">
-                <Form.Group controlId="questionInput">
-                  <Form.Control type="text" placeholder="Escreva a pergunta aqui..." />
-                </Form.Group>
-                <Card.Title className="mt-3">Alternativas</Card.Title>
-                <Accordion className="d-flex flex-column gap-2">
-                  {alternativas.map((alternativa, index) => (
-                    <CreateAlternativa
-                      key={index}
-                      placeholder={alternativa.placeholder}
-                      eventKey={alternativa.eventKey}
+              <Form id="questionForm" onSubmit={handleEdit}>
+                <Form.Group>
+                  <Form.Group controlId="questionInput">
+                    <Form.Control
+                      type="text"
+                      placeholder="Escreva a pergunta aqui..."
+                      value={questionText}
+                      onChange={(e) => setQuestionText(e.target.value)}
                     />
-                  ))}
-                </Accordion>
-                <Button variant="light" className="mt-3" id="addOptionButton" onClick={handleAddOption}>
-                  Adicionar opção
+                  </Form.Group>
+                  <Card.Title className="mt-3">Alternativas</Card.Title>
+                  <Accordion className="d-flex flex-column gap-2">
+                    {alternativaProps.map((alternativa) => (
+                      <CreateAlternativa
+                        key={alternativa.id}
+                        id={alternativa.id}
+                        placeholder={alternativa.placeholder}
+                        eventKey={alternativa.eventKey}
+                        onRemove={() => handleRemoveOption(alternativa.id)}
+                        tags={tags}
+                        onTextChange={handleAlternativaTextChange}
+                        onTagChange={handleAlternativaTagChange}
+                        initialText={alternativaTexts[alternativa.id]}
+                        initialTags={alternativaTags[alternativa.id]}
+                      />
+                    ))}
+                  </Accordion>
+                  <Button variant="light" className="mt-3" id="addOptionButton" onClick={handleAddOption}>
+                    Adicionar opção
+                  </Button>
+                </Form.Group>
+                <Button variant="success" type="submit" className="mt-3">
+                  Salvar
                 </Button>
               </Form>
-              <Button variant="success" type="submit" className="mt-3">
-                Salvar
-              </Button>
             </Card.Body>
           </Card>
         </Col>
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,5 @@
 import Dexie from "dexie";
-import { Alternativa, Caracteristica, Pergunta, Questionario, Tag, TipoCaracteristica } from "../types";
+import { Alternativa, AlternativaWithTags, Caracteristica, Pergunta, Questionario, Tag, TipoCaracteristica } from "../types";
 import { db } from "./db";
 
 // === CRUD Questionario ===
@@ -190,6 +190,51 @@ export const associateAlternativaToTags = async (id: number, tag_ids: number[]) 
     });
   } catch (error) {
     console.error("Erro ao associar alternativa com tags:", error);
+  }
+};
+
+/**
+ * Atualiza a associação de tags a uma alternativa
+ *
+ * @param {number} id_alternativa Id da alternativa
+ * @param {number[]} tag_ids Ids das tags a serem associadas
+ */
+export const updateAssociationAlternativeToTags = async (id_alternativa: number, tag_ids: number[]) => {
+  try {
+    await db.transaction("rw", db.alternativa_tag, async () => {
+      // Obtenha as associações existentes para a alternativa
+      const existingAssociations = await db.alternativa_tag.where("id_alternativa").equals(id_alternativa).toArray();
+
+      // Crie um Set com IDs fornecidos para facilitar a verificação
+      const tagIdsSet = new Set(tag_ids);
+
+      // Determine as associações a adicionar e remover
+      const currentTagIdsSet = new Set(existingAssociations.map((entry) => entry.id_tag));
+      const tagsToAdd = tag_ids.filter((tag_id) => !currentTagIdsSet.has(tag_id));
+      const tagsToRemove = existingAssociations
+        .filter((entry) => !tagIdsSet.has(entry.id_tag))
+        .map((entry) => entry.id_tag);
+
+      // Adicione novas associações
+      const newAssociations = tagsToAdd.map((tag_id) => ({
+        id_alternativa: id_alternativa,
+        id_tag: tag_id,
+      }));
+      if (newAssociations.length > 0) {
+        await db.alternativa_tag.bulkAdd(newAssociations);
+      }
+
+      // Remova associações antigas
+      if (tagsToRemove.length > 0) {
+        await db.alternativa_tag
+          .where("id_alternativa")
+          .equals(id_alternativa)
+          .and((entry) => tagsToRemove.includes(entry.id_tag))
+          .delete();
+      }
+    });
+  } catch (error) {
+    console.error("Erro ao atualizar associações de alternativas com tags:", error);
   }
 };
 
@@ -417,6 +462,116 @@ export const updateAssociationCaracteristicaToTags = async (id: number, tag_ids:
     console.error("Erro ao atualizar associações de características com tags:", error);
   }
 };
+
+
+export async function getAlternativasWithTagsByPergunta(pergunta: Pergunta): Promise<AlternativaWithTags[]> {
+  // Primeiro, buscamos todas as alternativas associadas à pergunta
+  const alternativas = await db.alternativa
+    .where("id_pergunta")
+    .equals(pergunta.id_pergunta!)
+    .toArray();
+
+  // Agora, para cada alternativa, buscamos as tags associadas
+  const alternativasWithTags: AlternativaWithTags[] = await Promise.all(
+    alternativas.map(async (alternativa) => {
+      const tags = await db.alternativa_tag
+        .where("id_alternativa")
+        .equals(alternativa.id_alternativa!)
+        .toArray();
+
+      const tagDetails: Tag[] = await Promise.all(
+        tags.map(async (altTag) => {
+          const tag = await db.tag.where("id_tag").equals(altTag.id_tag!).first();
+          return tag!;
+        })
+      );
+
+      return {
+        ...alternativa,
+        tags: tagDetails,
+      };
+    })
+  );
+
+  return alternativasWithTags;
+}
+
+
+/**
+ * Edita uma pergunta e suas alternativas, incluindo as associações de tags
+ *
+ * @param {Pergunta} pergunta Pergunta a ser editada
+ * @param {Alternativa[]} alternativas Lista de alternativas atualizada para a pergunta, incluindo tags
+ */
+export const editPerguntaEAlternativas = async (pergunta: Pergunta, alternativas: AlternativaWithTags[]) => {
+  try {
+    await db.transaction("rw", [db.pergunta, db.alternativa, db.alternativa_tag], async () => {
+      // Atualize o texto da pergunta
+      await db.pergunta.update(pergunta.id_pergunta, { pergunta: pergunta.pergunta });
+
+      // Obtenha as alternativas existentes para a pergunta
+      const existingAlternatives = await db.alternativa.where("id_pergunta").equals(pergunta.id_pergunta!).toArray();
+
+      // Crie um Set com IDs das alternativas fornecidas para facilitar a verificação
+      const providedAlternativeIds = new Set(alternativas.map((alt) => alt.id_alternativa));
+
+      // Determine as alternativas a adicionar, remover e editar
+      const alternativesToAdd = alternativas.filter((alt) => !alt.id_alternativa);
+      const alternativesToRemove = existingAlternatives.filter((existingAlt) => !providedAlternativeIds.has(existingAlt.id_alternativa));
+      const alternativesToUpdate = alternativas.filter((alt) => alt.id_alternativa && providedAlternativeIds.has(alt.id_alternativa));
+
+      // Adicione novas alternativas
+      if (alternativesToAdd.length > 0) {
+        const addedAlternativesIds = await db.alternativa.bulkAdd(alternativesToAdd.map((alt) => ({
+          id_pergunta: pergunta.id_pergunta!,
+          alternativa: alt.alternativa,
+        })), { allKeys: true });
+
+        // Adicionar associações de tags para as alternativas recém-criadas
+        for (let i = 0; i < addedAlternativesIds.length; i++) {
+          const newAltId = addedAlternativesIds[i];
+          const newAltTags = alternativesToAdd[i].tags.map(tag => tag.id_tag!);
+          if (newAltTags) {
+            await updateAlternativaTags(String(newAltId), newAltTags);
+          }
+        }
+      }
+
+      // Remova alternativas antigas e suas associações de tags
+      if (alternativesToRemove.length > 0) {
+        await db.alternativa.bulkDelete(alternativesToRemove.map((alt) => alt.id_alternativa));
+        for (const alt of alternativesToRemove) {
+          await db.alternativa_tag.where("id_alternativa").equals(alt.id_alternativa!).delete();
+        }
+      }
+
+      // Atualize alternativas existentes e suas associações de tags
+      for (const alt of alternativesToUpdate) {
+        await db.alternativa.update(alt.id_alternativa, { alternativa: alt.alternativa });
+        const updatedTags = alt.tags.map(tag => tag.id_tag!);
+        if (updatedTags) {
+          await updateAlternativaTags(String(alt.id_alternativa), updatedTags);
+        }
+      }
+    });
+  } catch (error) {
+    console.error("Erro ao editar pergunta e alternativas:", error);
+  }
+};
+
+export const updateAlternativaTags = async (id_alternativa: string, tags: number[]) => {
+  // Remove as associações de tags existentes para a alternativa
+  await db.alternativa_tag.where({ id_alternativa }).delete();
+
+  // Adiciona as novas associações de tags para a alternativa
+  const alternativaTags = tags.map((id_tag) => ({
+    id_alternativa: Number(id_alternativa),
+    id_tag,
+  }));
+
+  await db.alternativa_tag.bulkAdd(alternativaTags);
+};
+
 
 /**
  * Deleta uma característica do banco de dados
