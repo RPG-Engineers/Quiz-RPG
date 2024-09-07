e227f8baa4212fa7867f8f9f8fabb91feb15a398
Murilo <lilo.marino@hotmail.com>
2024-09-07 17:06:08 -0300

    feat: :sparkles: Implementado Ver Pergunta

--- src/components/CardPergunta.tsx
+++ src/components/CardPergunta.tsx
@@ -2,22 +2,24 @@ import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Pergunta } from "../types";
 
 interface CardPerguntaProps {
-    pergunta: string
+  pergunta: Pergunta;
+  handleEdit: (id: number) => void;
 }
 
-export const CardPergunta: React.FC<CardPerguntaProps> = ({pergunta}) => {
+export const CardPergunta: React.FC<CardPerguntaProps> = ({ pergunta, handleEdit }) => {
   return (
     <Container className="mt-3">
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
           <Card>
             <Card.Body className="d-flex align-items-center justify-content-between">
-              <h1 className="fs-5 text-break">{pergunta}</h1>
+              <h1 className="fs-5 text-break">{pergunta.pergunta}</h1>
               <div className="d-flex gap-2">
                 <Button variant="warning" className="text-white">
-                  <FontAwesomeIcon icon={faPen} />
+                  <FontAwesomeIcon icon={faPen} onClick={() => handleEdit(pergunta.id_pergunta!)} />
                 </Button>
                 <Button variant="danger">
                   <FontAwesomeIcon icon={faTrash} />
--- src/views/Perguntas.tsx
+++ src/views/Perguntas.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import { CardPergunta } from "../components/CardPergunta";
 import { CreatePergunta } from "../components/CreatePergunta";
+import { getPerguntas, getTags } from "../database/database";
+import { Pergunta, Tag } from "../types";
+import { useNavigate } from "react-router-dom";
 
 const Perguntas: React.FC = () => {
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [perguntas, setPerguntas] = useState<Pergunta[]>([]);
+  const navigate = useNavigate();
+
+  const handleEdit = (id: number) => {
+    navigate(`/editar-pergunta/${id}`);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const tagsFromDB = await getTags();
+      const perguntasFromDB = await getPerguntas();
+      setTags(tagsFromDB);
+      setPerguntas(perguntasFromDB);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <CreatePergunta></CreatePergunta>
-      <CardPergunta pergunta="Sua Pergunta?"></CardPergunta>
+      <CreatePergunta tags={tags} />
+      {perguntas.map((pergunta) => (
+        <CardPergunta key={pergunta.id_pergunta} pergunta={pergunta} handleEdit={handleEdit} />
+      ))}
     </>
   );
 };
