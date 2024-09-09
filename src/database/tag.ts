923d505a1270f50dcf8200e7b661e6cd3cd9b200
Murilo <lilo.marino@hotmail.com>
2024-09-08 22:48:05 -0300

    refactor: :recycle: Reorganizado e refatorado tag

--- src/database/tag.ts
+++ src/database/tag.ts
@@ -2,81 +2,97 @@ import Dexie from "dexie";
 import { Tag } from "../types";
 import { db } from "./db";
 
-// === CRUD Tag ===
 /**
  * Adiciona uma tag ao banco de dados
  *
  * @param {Tag} tag
  */
-
-export const addTag = async (tag: Tag) => {
+export async function addTag(tag: Tag) {
   await db.tag.add(tag);
-};
+}
+
 /**
  * Obtém todas as tags do banco de dados
  *
  */
-
-export const getTags = async (): Promise<Tag[]> => {
+export async function getTags(): Promise<Tag[]> {
   return await db.tag.toArray();
-};
+}
+
 /**
  * Obtém uma tag do banco de dados
  *
  * @param {number} id Id da tag a ser obtida
  */
-
-export const getTagById = async (id: number): Promise<Tag> => {
+export async function getTagById(id: number): Promise<Tag> {
   const tag = await db.tag.get(id);
-  if (typeof tag === "undefined") {
+  if (tag === undefined || tag === null) {
     throw new Error("Tag não encontrada");
   }
   return tag;
-};
+}
 
 /**
  * Obtém as tags de uma determinada característica
  *
- * @param {number} id_caracteristica Id da característica
+ * @param {number} caracteristicaId Id da característica
  */
-
-export const getTagsByCaracteristicaId = async (id_caracteristica: number): Promise<Tag[]> => {
+export async function getTagsByCaracteristicaId(caracteristicaId: number): Promise<Tag[]> {
   return await db.transaction("r", db.caracteristica_tag, db.tag, async () => {
-    const tags_ids = await db.caracteristica_tag.where("id_caracteristica").equals(id_caracteristica).toArray();
+    const tagsIds = await db.caracteristica_tag.where("id_caracteristica").equals(caracteristicaId).toArray();
     const tags = await Dexie.Promise.all(
-      tags_ids.map(async (tags_carac) => {
+      tagsIds.map(async (tags_carac) => {
         const tag = await db.tag.get(tags_carac.id_tag);
-        if (typeof tag === "undefined") {
-          throw new Error("Tag não encontrada");
+        if (tag === undefined || tag === null) {
+          throw new Error("Tag não encontrada, dados inconsistentes");
         }
         return tag;
       })
     );
     return tags;
   });
-};
+}
+
+/**
+ * Obtém as tags de uma determinada alternativa
+ *
+ * @param {number} alternativaId Id da alternativa
+ */
+export async function getTagsByAlternativaId(alternativaId: number): Promise<Tag[]> {
+  return await db.transaction("r", db.alternativa_tag, db.tag, async () => {
+    const tagsIds = await db.alternativa_tag.where("id_alternativa").equals(alternativaId).toArray();
+    const tags = await Dexie.Promise.all(
+      tagsIds.map(async (tags_alt) => {
+        const tag = await db.tag.get(tags_alt.id_tag);
+        if (tag === undefined || tag === null) {
+          throw new Error("Tag não encontrada, dados inconsistentes");
+        }
+        return tag;
+      })
+    );
+    return tags;
+  });
+}
 
 /**
  * Atualiza uma tag do banco de dados
  *
  * @param {number} id Id da tag a ser atualizada
- * @param {Tag} updated_tag Tag atualizada
+ * @param {Tag} updatedTag Tag atualizada
  */
-
-export const updateTag = async (id: number, updated_tag: Tag) => {
-  await db.tag.update(id, updated_tag);
-};
+export async function updateTag(id: number, updatedTag: Tag) {
+  await db.tag.update(id, updatedTag);
+}
 
 /**
  * Deleta uma tag do banco de dados
  *
  * @param {number} id Id da tag a ser deletada
  */
-
-export const deleteTag = async (id: number) => {
+export async function deleteTag(id: number) {
   await db.transaction("rw", db.tag, db.caracteristica_tag, db.alternativa_tag, async () => {
     await db.tag.delete(id);
     await db.caracteristica_tag.where("id_tag").equals(id).delete();
     await db.alternativa_tag.where("id_tag").equals(id).delete();
   });
-};
+}
