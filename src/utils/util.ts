02032fce6677641aa0dcbe84613fe8c475cc2b07
Murilo <lilo.marino@hotmail.com>
2024-09-13 22:31:52 -0300

    refactor: :recycle: Refatorado trafegação e exibição de resultado

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,3 @@
-import { CardType } from "../components/CardResultado";
 import { getAlternativasByPerguntaId } from "../database/alternativa";
 import { getCaracteristicas } from "../database/caracteristica";
 import { CaracteristicaWithTags, TipoCaracteristica } from "../types";
@@ -24,25 +23,14 @@ export async function calcularResultado(resultsMap: Map<number, number>) {
   // Calcula a pontuação de cada característica
   const caracteristicaMap: Map<CaracteristicaWithTags, number> = await calculateCaracteristicaPontuation(tagsMap);
 
-  // Converta o Map em um array de entradas
-  const caracteristicasArray = Array.from(caracteristicaMap.entries());
+  // Converte o Map em um array de entradas e ordena pelo valor da pontuação (decrescente)
+  const resultado = Array.from(caracteristicaMap.entries()).sort(
+    ([_objA, pontuacaoA], [_objB, pontuacaoB]) => pontuacaoB - pontuacaoA
+  );
 
-  // Função para filtrar, ordenar e pegar os 3 principais para uma categoria específica
-  const getTop3ByCategory = (tipo: TipoCaracteristica) => {
-    const filtradoArray = caracteristicasArray.filter(([caracteristica, _]) => {
-      return caracteristica.tipo === tipo;
-    });
-    const ordenadoArray = filtradoArray.sort(([_objA, pontuacaoA], [_objB, pontuacaoB]) => pontuacaoB - pontuacaoA);
-    return ordenadoArray.slice(0, 3);
-  };
-
-  // Obter os 3 principais resultados para cada categoria
-  const TopClasse = getTop3ByCategory(TipoCaracteristica.CLASSE);
-  const TopRaca = getTop3ByCategory(TipoCaracteristica.RACA);
-  const TopBackground = getTop3ByCategory(TipoCaracteristica.BACKGROUND);
-
-  return { TopClasse, TopRaca, TopBackground };
+  return resultado;
 }
+
 
 async function calculateTagPontuation(resultsMap: Map<number, number>) {
   const tagsMap: Map<number, number> = new Map();
@@ -89,17 +77,4 @@ async function calculateCaracteristicaPontuation(tagsMap: Map<number, number>) {
   }
 
   return caracteristicaMap;
-}
-
-export const getCardType = (index: number): CardType => {
-  switch (index) {
-    case 0:
-      return CardType.Gold;
-    case 1:
-      return CardType.Silver;
-    case 2:
-      return CardType.Bronze;
-    default:
-      return CardType.Bronze; // Default fallback
-  }
 }
