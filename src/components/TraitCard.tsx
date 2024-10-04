168aace5d086fb056d3769fde3fbe44713da9178
Murilo <lilo.marino@hotmail.com>
2024-10-04 18:59:39 -0300

    refactor: :recycle: Refatorado TraitCard e TraitEdit para usar react-bootstrap

--- src/components/TraitCard.tsx
+++ src/components/TraitCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { CaracteristicaWithTags, TipoCaracteristica } from "../types";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { TagComponent } from "./TagComponent";
 
 interface TraitCardProps {
   tipo: TipoCaracteristica;
@@ -8,48 +10,46 @@ interface TraitCardProps {
 
 const TraitCard: React.FC<TraitCardProps> = ({ tipo, caracteristicas }) => {
   return (
-    <div className="container mt-5">
-      <div className="row">
+    <Container className="mt-5">
+      <Row>
         {caracteristicas
           .filter((caracteristica) => caracteristica.tipo === tipo)
           .map((caracteristica) => (
-            <div key={caracteristica.id_caracteristica} className="col-md-4 mb-4">
-              <div className="card mb-3" style={{ maxWidth: "540px", height: "300px" }}>
-                <div className="row no-gutters" style={{ height: "100%" }}>
-                  <div className="col-md-8 d-flex flex-column">
-                    <h5 className="card-title">{caracteristica.nome}</h5>
-                    <h6>Descrição</h6>
-                    <div
-                      className="card-body"
-                      style={{
-                        overflowY: "auto",
-                        maxHeight: "200px",
-                        scrollbarWidth: "none", // Firefox
-                        msOverflowStyle: "none", // IE and Edge
-                      }}
-                    >
-                      <p className="card-text">{caracteristica.descricao}</p>
-                    </div>
-                    {caracteristica.tags.map((tag) => (
-                      <span key={tag.id_tag} className="badge" style={{ backgroundColor: tag.cor, color: "white" }}>
-                        {tag.nome}
-                      </span>
-                    ))}
-                  </div>
-                  <div className="col-md-4">
-                    <img
+            <Col key={caracteristica.id_caracteristica} md={4} className="mb-4">
+              <Card className="mb-3" style={{ maxWidth: "540px", height: "300px" }}>
+                <Row noGutters style={{ height: "100%" }}>
+                  <Col md={8} className="d-flex flex-column">
+                    <Card.Body>
+                      <Card.Title>{caracteristica.nome}</Card.Title>
+                      <h6>Descrição</h6>
+                      <div
+                        style={{
+                          overflowY: "auto",
+                          maxHeight: "200px",
+                          scrollbarWidth: "none", // Firefox
+                          msOverflowStyle: "none", // IE and Edge
+                        }}
+                      >
+                        <Card.Text>{caracteristica.descricao}</Card.Text>
+                      </div>
+                      {caracteristica.tags.map((tag) => (
+                        <TagComponent key={tag.id_tag} selectable={false} tag={tag} />
+                      ))}
+                    </Card.Body>
+                  </Col>
+                  <Col md={4}>
+                    <Card.Img
                       src={caracteristica.url_imagem}
-                      className="card-img"
                       alt={caracteristica.nome}
                       style={{ objectFit: "cover", height: "100%" }}
                     />
-                  </div>
-                </div>
-              </div>
-            </div>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
           ))}
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 };
 
