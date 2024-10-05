651836b6281c5b981307fe82af8e320a355000b5
Murilo <lilo.marino@hotmail.com>
2024-10-04 21:26:47 -0300

    refactor: :recycle: Componentizado view de EditTag

---
+++ src/components/TagEdit.tsx
@@ -0,0 +1,76 @@
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import { Tag } from "../types";
+import { useEffect, useState } from "react";
+import { getTagById, updateTag } from "../database/tag";
+import { useNavigate } from "react-router-dom";
+
+interface TagEditProps {
+  id: number;
+}
+
+const TagEdit: React.FC<TagEditProps> = ({ id }) => {
+  const [nome, setNome] = useState("");
+  const [cor, setCor] = useState("#000000");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchTagData = async () => {
+      const tag = await getTagById(id);
+      setNome(tag.nome);
+      setCor(tag.cor);
+    };
+
+    fetchTagData();
+  }, [id]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const updatedTag: Tag = {
+      nome,
+      cor,
+    };
+
+    await updateTag(id, updatedTag);
+    navigate("/tags");
+  };
+
+  return (
+    <Container className="h-100 mt-3">
+      <Row className="align-items-center h-100">
+        <Col xs={12} md={6} className="mx-auto">
+          <Card>
+            <Card.Body>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="nome">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    placeholder="Digite o nome da tag"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="cor" className="mt-2">
+                  <Form.Label>Cor</Form.Label>
+                  <Form.Control type="color" value={cor} onChange={(e) => setCor(e.target.value)} />
+                </Form.Group>
+
+                <Button type="submit" variant="success" className="mt-3">
+                  Salvar
+                </Button>
+              </Form>
+
+              <h5 className="mt-4">Pré-visualização:</h5>
+              <span id="preview" className="badge" style={{ backgroundColor: cor, color: "white" }}>
+                {nome || "Nome do Badge"}
+              </span>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TagEdit;
