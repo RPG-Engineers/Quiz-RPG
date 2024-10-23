ec92b7f78d13ea19b4edf90d535e38c95b6e0ff7
LiloMarino <lilo.marino@hotmail.com>
2024-10-23 12:53:14 -0300

    fix: :wrench: Corrigido barra de pesquisa em características
    Closes #156

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/custom.css';
 import "./assets/styles/resultado.css";
+import "./assets/styles/searchBar.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
