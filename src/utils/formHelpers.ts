47114c1551aad1d7b4841af5d2ab8d35c25dd00a
LiloMarino <lilo.marino@hotmail.com>
2024-10-24 16:24:15 -0300

    feat: :sparkles: Implementado efeitos de erro no input para indicar erros

---
+++ src/utils/formHelpers.ts
@@ -0,0 +1,22 @@
+// formHelpers.ts
+import { Dispatch, SetStateAction } from "react";
+import { FormErrors } from "../types";
+
+// Função genérica para manipular mudanças em formulários
+export const handleInputChange = <T>(
+  e: React.ChangeEvent<HTMLInputElement>,
+  formData: T,
+  setFormData: Dispatch<SetStateAction<T>>,
+  formErrors: FormErrors,
+  setFormErrors: Dispatch<SetStateAction<FormErrors>>
+) => {
+  const { name, value } = e.target;
+
+  // Atualiza o estado do formulário com o novo valor
+  setFormData({ ...formData, [name]: value });
+
+  // Reseta o erro no campo correspondente, se houver
+  if (formErrors[name]) {
+    setFormErrors({ ...formErrors, [name]: false });
+  }
+};
