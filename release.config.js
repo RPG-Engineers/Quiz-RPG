dc099227b7e2c7b43bc3230bb86c4a5e301bba3d
Murilo <lilo.marino@hotmail.com>
2024-10-25 23:11:23 -0300

    feat: :sparkles: Adicionado pop-up de patch notes

---
+++ release.config.js
@@ -0,0 +1,30 @@
+import { writeFileSync } from 'fs';
+
+const generatePatchNotes = {
+  generateNotes: async (pluginConfig, context) => {
+    const { nextRelease } = context;
+    const patchNotes = {
+      version: nextRelease.version,
+      notes: nextRelease.notes,
+      date: new Date().toISOString(),
+    };
+
+    // Grava o arquivo JSON na pasta public
+    console.log('Gerando patch notes...');
+    writeFileSync('public/patch-notes.json', JSON.stringify(patchNotes, null, 2));
+    console.log('Patch notes gerados com sucesso em public/patch-notes.json');
+  },
+};
+
+export default {
+  branches: ['main', 'develop'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { npmPublish: false }],
+    '@semantic-release/github',
+    generatePatchNotes,
+    '@semantic-release/changelog',
+    ['@semantic-release/git', { assets: ['CHANGELOG.md', 'package.json', 'public/patch-notes.json'] }],
+  ],
+};
