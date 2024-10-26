fde11d7818f3e436fbf8913a58b7b1c1191b8f71
Murilo <lilo.marino@hotmail.com>
2024-10-26 15:07:16 -0300

    fix: :wrench: Corrigido problema de dependência do Toast em App

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/custom.css';
 import "./assets/styles/resultado.css";
 import "./assets/styles/searchBar.css";
-import { ToastProvider } from './context/ToastContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastProvider>
       <App />
-    </ToastProvider>
   </React.StrictMode>,
 )
