d896ae8a6261846adb3f47c3ab65bd9d87ad887d
Murilo <lilo.marino@hotmail.com>
2024-10-26 14:04:32 -0300

    fix: :wrench: Corrigido apresentação das notas de atualização

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/custom.css';
 import "./assets/styles/resultado.css";
 import "./assets/styles/searchBar.css";
+import { ToastProvider } from './context/ToastContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
 )
