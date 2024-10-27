8a9a0c9f6b464d6cb62d417f07b1df8e9c3330f8
Murilo <lilo.marino@hotmail.com>
2024-10-27 16:08:16 -0300

    style: :lipstick: Alterado estilo do resultado

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -2,19 +2,29 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { CardType } from "../types";
 import silhueta from "../assets/img/silhueta.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 interface ResultCardProps {
   title: string;
   imageSrc: string | undefined;
   type: CardType;
+  placement: number;
 }
 
-export const ResultCard: React.FC<ResultCardProps> = ({ title, imageSrc, type }) => {
+export const ResultCard: React.FC<ResultCardProps> = ({ title, imageSrc, type, placement }) => {
   return (
     <Card className={`text-center card-resultado ${type}`}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={imageSrc || silhueta} alt={`Imagem ${type}`} />
+
+        {/* Centralizar o ícone e o número */}
+        <div className="trophy-container">
+          <div className="laurel-background"></div>
+          <FontAwesomeIcon icon={faTrophy} className={`trophy-icon ${type}`} />
+          <span className="placement-number">{placement}</span>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/components/ResultPodium.tsx
+++ src/components/ResultPodium.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap"; 
 import { ResultCard } from "./ResultCard";
 import { CaracteristicaWithTags, CardType } from "../types";
 
@@ -15,6 +15,7 @@ const ResultPodium: React.FC<ResultPodiumProps> = ({ top3 }) => {
             title={top3[2]?.nome}
             imageSrc={top3[2]?.url_imagem}
             type={CardType.Bronze}
+            placement={3}
           />
         </Col>
         <Col xs={4}>
@@ -22,6 +23,7 @@ const ResultPodium: React.FC<ResultPodiumProps> = ({ top3 }) => {
             title={top3[0]?.nome}
             imageSrc={top3[0]?.url_imagem}
             type={CardType.Gold}
+            placement={1}
           />
         </Col>
         <Col xs={4}>
@@ -29,6 +31,7 @@ const ResultPodium: React.FC<ResultPodiumProps> = ({ top3 }) => {
             title={top3[1]?.nome}
             imageSrc={top3[1]?.url_imagem}
             type={CardType.Silver}
+            placement={2}
           />
         </Col>
       </Row>
