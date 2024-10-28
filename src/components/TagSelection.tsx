b01043fe912c9a8c4a29b256f1dee3504aff8cbb
Murilo <lilo.marino@hotmail.com>
2024-10-27 21:15:26 -0300

    feat: :sparkles: Colocado em ordem alfabética seleção de tags

--- src/components/TagSelection.tsx
+++ src/components/TagSelection.tsx
@@ -9,9 +9,12 @@ interface TagSelectionProps {
 }
 
 export const TagSelection: React.FC<TagSelectionProps> = ({ tags, selectedTags, handleTagToggle }) => {
+  // Ordena os tags em ordem alfabética pelo nome
+  const sortedTags = [...tags].sort((a, b) => a.nome.localeCompare(b.nome));
+
   return (
     <div className="d-flex gap-1 flex-wrap badge-container mt-1">
-      {tags.map((tag) => (
+      {sortedTags.map((tag) => (
         <TagComponent
           key={tag.id_tag}
           tag={tag}
